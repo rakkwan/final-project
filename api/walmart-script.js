bbf9c64e3622046b0665e6713f4e71b91075a5dc
Max Lee <maxwell.a.lee56@gmail.com>
2019-05-18 16:11:02 -0400

    Made a search possible with enter and show total items found

--- api/walmart-script.js
+++ api/walmart-script.js
@@ -7,16 +7,32 @@
  */
 
 // Onload call
-window.onload = setForm;
+// window.onload = setForm;
+
+$("#btnSearch").on("click", setForm);
+
+$("#product").on("keydown", function(event) {
+    var code;
+
+    if (event.key !== undefined) {
+        code = event.key;
+    }
+
+    if(code === "Enter")
+    {
+        setForm();
+    }
+});
 
 function setForm()
 {
-    //walmart buying section
-    // create an empty array called prices
-    var prices = [];
-    // create an empty array called items
-    var items = [];
-    $("#btnSearch").on("click", function() {
+    if(document.readyState === "complete") {
+        //walmart buying section
+        // create an empty array called prices
+        var prices = [];
+        // create an empty array called items
+        var items = [];
+
         var output = $("#output");
         var product = $("#product").val();
 
@@ -36,32 +52,32 @@ function setForm()
 
             // before requesting data set cookie values
             // indicates whether or not cross-site Access-Control requests should be made using credentials
-            beforeSend: function(xhr){
+            beforeSend: function (xhr) {
                 xhr.withCredentials = true;
             }
-        }).done(function(data){ // run this function if request was successful
+        }).done(function (data) { // run this function if request was successful
             // takes a JSON string and transforms it into a JavaScript object
             data = JSON.parse(data);
 
-            //print the name of an item to the div            
-            output.html(data.query + ": <br>");
+            //print the name of an item to the div
+            output.html(data.items.length+" results for: "+data.query + "<br>");
             $("#outputTable").html("<tr><th>Image</th><th>Description</th><th>Price</th><th>Buy It</th></tr>");
-            for (var i = 0; i < data.items.length; i++){
+            for (var i = 0; i < data.items.length; i++) {
                 items[i] = data.items[i].name;
                 prices[i] = data.items[i].salePrice;
-                $("#outputTable").append('<tr><td><img src="'+data.items[i].mediumImage+'"></td><td>'+data.items[i].name+
-                    '</td><td>$'+data.items[i].salePrice+'</td><td><button id="buy'+i+'" ' +
+                $("#outputTable").append('<tr><td><img src="' + data.items[i].mediumImage + '"></td><td>' + data.items[i].name +
+                    '</td><td>$' + data.items[i].salePrice + '</td><td><button id="buy' + i + '" ' +
                     'class="buying">Buy</button></td></tr>');
             }
 
-            // buy button section           
-            $(".buying").on("click", function() {
-                var index = $(this).attr("id").substring(3,4);
+            // buy button section
+            $(".buying").on("click", function () {
+                var index = $(this).attr("id").substring(3, 4);
 
                 // list of items in your cart
                 $("ol").append("<li>" + items[index] + "</li>");
             });
         });
-    });
+    }
 }
 
