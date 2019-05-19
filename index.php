8167a3c48f44d8fa4b570729c26af42b577c63de
Jittima Goodrich <jgoodrich4@mail.greenriver.edu>
2019-05-19 03:12:50 -0400

    Add register link to home page

--- index.php
+++ index.php
@@ -21,9 +21,32 @@ $f3->route('GET /', function()
     echo $view->render('views/home.html');
 });
 
-//define a loggin route
-$f3->route('GET|POST /loggin', function ()
+//define a register route
+$f3->route('GET|POST /register', function($f3)
 {
+    $view = new Template();
+    echo $view->render('views/register.html');
+});
+
+//define a loggin route
+$f3->route('GET|POST /loggin', function ($f3)
+{
+    if(!empty($_POST))
+    {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $f3->set('username', $username);
+        $f3->set('password', $password);
+
+        /*
+        if(valid())
+        {
+
+        }
+        */
+        $f3->reroute('views/profile.html');
+    }
     $view = new Template();
     echo $view->render('views/loggin.html');
 });
