69ebcf643878074a602d30fcdb1fad7660902dfc
Jittima Goodrich <jgoodrich4@mail.greenriver.edu>
2019-05-20 10:39:16 -0400

    Fixing routes

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ error_reporting(E_ALL);
 //Create an instance of the Base class
 $f3 = Base::instance();
 
+
 //define a default route
 $f3->route('GET /', function()
 {
@@ -39,24 +40,26 @@ $f3->route('GET|POST /loggin', function ($f3)
         $f3->set('username', $username);
         $f3->set('password', $password);
 
-        /*
+
         if(valid())
         {
 
         }
-        */
+
         $f3->reroute('views/profile.html');
     }
     $view = new Template();
     echo $view->render('views/loggin.html');
 });
 
+
 //define a search page route
-$f3->route('GET|POST /search', function ()
+$f3->route('GET|POST /search', function ($f3)
 {
     if(!empty($_POST))
     {
         $cartItem = new CartItem();
+        $f3->reroute('/summary');
     }
 
     $view = new Template();
